5bf91051712b45d7c97f6d6d25bfee08b6e01179
Surya22072008 <78208952+Surya22072008@users.noreply.github.com>
2022-01-10 17:19:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,10 +10,41 @@ function setup() {
 	instializeInSetup(mario);
 }
 
+function setup(){
+	createCanvas(650,400);
+	video  = createCapture(VIDEO);
+	video.size(600,300);
+	poseNet=ml5.posenet(video,modelloaded);
+	poseNet.on('pose',getposes)
+}
 function draw() {
 	game()
+	background("#D3D3D3");
+if(noseX<300){
+marioX=marioX-1;
 }
-
+if(noseX>300){
+	marioX=marioX+1;
+	}
+	if(noseY<150){
+	marioY=marioY-1;
+		}
+		if(noseY>150){
+	marioY=marioY+1;
+			}
+image(img,marioX,marioY,40,70);
+}
+function modelloaded()
+{
+	console.log("Posenet is initialized ")
+}
+function getposes(results){
+if(results.length>0){
+	noseX=results[0].pose.nose.x;
+	noseY=results[0].pose.nose.y;
+	console.log("noseX = "+noseX+" noseY = "+noseY);
+}
+}
 
 
 
